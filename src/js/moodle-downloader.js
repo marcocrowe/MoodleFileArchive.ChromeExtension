a169b425497f5b014d3379e7044d9a5e4737eb63
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2024-02-24 17:02:11 +0000

    getFileExtensionFromResponse QueryString fix

--- src/js/moodle-downloader.js
+++ src/js/moodle-downloader.js
@@ -36,8 +36,7 @@ class MoodleDownloader {
 			const response = await fetch(moodleResource.url, options);
 			const blob = await response.blob();
 
-			const contentType = response.headers.get('content-type');
-			const extension = MoodleDownloader.getFileExtensionFromContentType(contentType);
+			const extension = MoodleDownloader.getFileExtensionFromResponse(response);
 
 			const hyperlinkElement = document.createElement('a');
 			hyperlinkElement.href = URL.createObjectURL(blob);
@@ -49,6 +48,22 @@ class MoodleDownloader {
 	}
 
 	/**
+	 * Extracts the file extension from the Content-Disposition header.
+	 * @param {string} contentDisposition - The Content-Disposition header.
+	 * @returns {string} The file extension if found, otherwise an empty string.
+	 */
+	static getFileExtensionFromContentDisposition(contentDisposition) {
+		const regex = /filename="(.+?)"/;
+		const match = regex.exec(contentDisposition);
+		if (match) {
+			const fileName = match[1];
+			const extensionIndex = fileName.lastIndexOf('.');
+			if (extensionIndex !== -1)
+				return fileName.slice(extensionIndex + 1);
+		}
+		return null;
+	}
+	/**
 	 * Get the file extension from the Content-Type header.
 	 * @param {string} contentType - The content type header.
 	 * @returns {string} The file extension if present otherwise returns an empty string if Content-Type is malformed.
@@ -58,25 +73,98 @@ class MoodleDownloader {
 			return '';
 
 		const mimeTypeToExtensionMap = {
-			'application/pdf': 'pdf',
-			'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
-			'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+			'application/vnd.ms-powerpoint': 'ppt',
+			'application/vnd.ms-powerpoint.presentation.macroenabled.12': 'pptm',
 			'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
-			'application/msaccess': 'mdb',
+			'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+			'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
 			'application/vnd.visio': 'vsdx',
-			'text/html;charset=UTF-8': 'html',
+			'application/msaccess': 'mdb',
+			'application/msexcel': 'xls',
+			'application/msword': 'doc',
+			'application/pdf': 'pdf',
+			'audio/mpeg': 'mp3',
+			'image/gif': 'gif',
+			'image/jpeg': 'jpg',
+			'image/png': 'png',
+			'text/plain': 'txt',
+			'text/html': 'html',
+			'text/html;charset=utf-8': 'html',
+			'text/html; charset=utf-8': 'html',
+			'video/mp4': 'mp4',
 			// Add more MIME types to file extension mappings as needed
 		};
 
-		const extensionFromMapping = mimeTypeToExtensionMap[contentType];
-		if (extensionFromMapping)
-			return extensionFromMapping;
+		const extension = mimeTypeToExtensionMap[contentType.toLowerCase()];
+		if (extension)
+			return extension;
 
 		const mimeTypeParts = contentType.split('/');
 		if (mimeTypeParts.length === 2) {
 			return mimeTypeParts[1];
 		}
-		return '';
+		return null;
+	}
+
+	/**
+	 * Get the file extension from the response object.
+	 * Tries to extract the extension from Content-Disposition header first,
+	 * then from the response URL, and finally from Content-Type header.
+	 * @param {Response} response - The response object.
+	 * @returns {string} The file extension if found, otherwise an empty string.
+	 */
+	static getFileExtensionFromResponse(response) {
+		const contentDisposition = response.headers.get('Content-Disposition');
+		if (contentDisposition) {
+			const extension = MoodleDownloader.getFileExtensionFromContentDisposition(contentDisposition);
+			if (extension)
+				return extension;
+		}
+
+		const url = response.url;
+		if (url) {
+			const extension = MoodleDownloader.getFileExtensionFromUrlQueryString(url);
+			if (extension)
+				return extension;
+		}
+
+		const contentType = response.headers.get('Content-Type');
+		return this.getFileExtensionFromContentType(contentType);
+	}
+
+	/**
+	 * Extracts the file extension from the 'response-content-disposition' parameter in a URL.
+	 * @param {string} url - The URL containing the 'response-content-disposition' parameter.
+	 * @returns {string|null} The file extension extracted from the URL, or null if not found or if the URL format is invalid.
+	 */
+	static getFileExtensionFromUrlQueryString(url) {
+		const dispositionStartIndex = url.indexOf('response-content-disposition=');
+
+		if (dispositionStartIndex !== -1) {
+			// Extract the substring containing the 'response-content-disposition' parameter and its value
+			const queryString = url.substring(dispositionStartIndex);
+
+			const filenameStartIndex = queryString.indexOf('filename%3D%22');
+
+			if (filenameStartIndex !== -1) {
+				const filenameSubstring = queryString.substring(filenameStartIndex + 'filename%3D'.length);
+
+				const filenameEndIndex = filenameSubstring.indexOf('%22&');
+
+				// Extract the filename part before any query parameters
+				const filenameEncoded = filenameEndIndex !== -1 ? filenameSubstring.substring(0, filenameEndIndex) : filenameSubstring;
+
+				const filename = decodeURIComponent(filenameEncoded.replace(/\+/g, ' '));
+
+				const dotIndex = filename.lastIndexOf('.');
+
+				// If a dot is found and it's not the last character in the filename
+				if (dotIndex !== -1 && dotIndex !== filename.length - 1) {
+					return filename.substring(dotIndex + 1);
+				}
+			}
+		}
+		return null;
 	}
 
 	/**
